1e63b7cc10495b17712f2871b701a9b739985152
Shreeram N S <nsshreeram@gmail.com>
2024-05-13 23:43:13 +0530

    Exception Handling

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 ##exception handling
 import sys
+from src.logger import logging
 
 def error_message_details(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
@@ -9,10 +10,12 @@ def error_message_details(error,error_details:sys):
     )
     return error_message  
     
-    class CustomException(Exception):
-        def __init__(self, error_message,error_details:sys):
-            super.__init__(error_message)
-            self.error_message = error_message_details(error_message,error_details=error_details)
+class CustomException(Exception):
+    def __init__(self, error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,error_details=error_details)
             
-        def __str__(self) :
-            return self.error_message
+    def __str__(self) :
+        return self.error_message
+    
+    
